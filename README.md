3e25717a02660389565e6b6df099e2442d87c814
Adrien Wehrung <a.wehrung@finatix.de>
2023-03-30 11:43:27 +0200

    add skeleton of 2 spring boot services, add rabbitmq instance and docker-compose setup

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # spring-boot-reactive-rabbitmq-tracing-example
+
+```
+mvn clean install
+docker-compose build
+docker-compose up -d
+```
+
+
+Upstream-service: localhost:12001
+
+Downstream-service: localhost:12002
+
+RabbitMQ admin overview: http://localhost:15672/
---
+++ upstream-service/src/main/java/com/example/downstream/UpstreamApplication.java
@@ -0,0 +1,11 @@
+package com.example.downstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpstreamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UpstreamApplication.class, args);
+    }
+}
